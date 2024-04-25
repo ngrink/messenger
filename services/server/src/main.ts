0ced7e89115dd5c81c0ee898118a52e56930aabc
Nikolay Grinko <ngrink.io@gmail.com>
2024-04-25 20:04:19 +0300

    chore: add swagger plugin, validation pipe

--- services/server/src/main.ts
+++ services/server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Messenger App')
+    .setDescription('API specification')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
+  await app.listen(7000);
 }
 bootstrap();
