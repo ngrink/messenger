07567493b957fd81c7f42c478d1e31f507b8765f
Nikolay Grinko <ngrink.io@gmail.com>
2024-04-25 20:12:09 +0300

    refactor: rename user model to account

--- services/server/prisma/migrations/20240425135536_set_optional_fields/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "profiles" ALTER COLUMN "avatar" DROP NOT NULL,
-ALTER COLUMN "description" DROP NOT NULL,
-ALTER COLUMN "birthdate" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "username" DROP NOT NULL;
--- services/server/prisma/migrations/20240425175402_init/migration.sql
+++ services/server/prisma/migrations/20240425175402_init/migration.sql
@@ -8,27 +8,27 @@ CREATE TYPE "MemberRole" AS ENUM ('OWNER', 'ADMIN', 'USER');
 CREATE TYPE "AttachmentType" AS ENUM ('PHOTO', 'VIDEO', 'AUDIO', 'DOCUMENT', 'PRESENTATION', 'SPREADSHEET', 'ARCHIVE', 'FILE');
 
 -- CreateTable
-CREATE TABLE "users" (
+CREATE TABLE "accounts" (
     "id" SERIAL NOT NULL,
-    "username" TEXT NOT NULL,
+    "username" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "profiles" (
-    "userId" INTEGER NOT NULL,
+    "accountId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
-    "avatar" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "birthdate" TIMESTAMP(3) NOT NULL,
+    "avatar" TEXT,
+    "description" TEXT,
+    "birthdate" TIMESTAMP(3),
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "profiles_pkey" PRIMARY KEY ("userId")
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("accountId")
 );
 
 -- CreateTable
@@ -46,17 +46,17 @@ CREATE TABLE "chats" (
 -- CreateTable
 CREATE TABLE "members" (
     "chatId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "accountId" INTEGER NOT NULL,
     "role" "MemberRole" NOT NULL,
 
-    CONSTRAINT "members_pkey" PRIMARY KEY ("chatId","userId")
+    CONSTRAINT "members_pkey" PRIMARY KEY ("chatId","accountId")
 );
 
 -- CreateTable
 CREATE TABLE "messages" (
     "id" SERIAL NOT NULL,
     "chatId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "accountId" INTEGER NOT NULL,
     "text" TEXT NOT NULL,
 
     CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
@@ -73,22 +73,22 @@ CREATE TABLE "attachments" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+CREATE UNIQUE INDEX "accounts_username_key" ON "accounts"("username");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+CREATE UNIQUE INDEX "accounts_email_key" ON "accounts"("email");
 
 -- AddForeignKey
-ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "members" ADD CONSTRAINT "members_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "members" ADD CONSTRAINT "members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "members" ADD CONSTRAINT "members_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "messages" ADD CONSTRAINT "messages_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "messages" ADD CONSTRAINT "messages_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
