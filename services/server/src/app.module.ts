aaea31569cecfdddcc3de214dcc10d562572a0c5
Nikolay Grinko <ngrink.io@gmail.com>
2024-04-26 01:56:37 +0300

    chore: add config module

--- services/server/src/app.module.ts
+++ services/server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './config/prisma.config';
 import { AccountsModule } from './shared/modules/accounts/accounts.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PrismaModule,
     AccountsModule
   ],
