cc6c0c91dda33d1b9b2156514b0322ef93af9095
Nikolay Grinko <ngrink.io@gmail.com>
2024-04-27 01:04:29 +0300

    feat(web): add react-router library, initial structure

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -1,6 +1,22 @@
+import React, { Suspense } from "react"
+import { Route, Routes } from "react-router-dom"
+
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route>
+            <Route path="/login" element={<div>LoginScreen</div>} />
+          </Route>
+          <Route>
+            <Route path="/" element={<div>HomeScreen</div>} />
+            <Route path="/chats/:id" element={<div>ChatScreen</div>} />
+          </Route>
+					<Route path="*" element={<h1>404 Not found</h1>} />
+        </Routes>
+      </Suspense>
+    </div>
   )
 }
 
